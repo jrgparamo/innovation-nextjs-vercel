908924750841dcdfdbe3ed4ae39811b8a38e29ff
Jorge Paramo <jorge.paramo@newbalance.com>
2024-01-21 23:35:23 -0600

    Preparing the fixed version of the dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -16,34 +16,24 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <div className={styles.shape}/>
           <p className={`${lusi.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to Feature 9 Updated.</strong> In this example, we load the page quicker by loading the most readily available components first.
+          </p>
+          <p className={`${lusi.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            The user can view/interact with other parts of the page while the rest of the parts are loading.
           </p>
           <Link
-            href="/login"
+            href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>To Dashboard</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          <Image
-            src="/hero-desktop.png"
-            width={1000}
-            height={760}
-            className="hidden md:block"
-            alt="Screenshots of the dashboard project showing desktop version"
-          />
-          <Image
-            src="/hero-mobile.png"
-            width={560}
-            height={620}
-            className="block md:hidden"
-            alt="Screenshots of the dashboard project showing desktop version"
-          />
+          <p>In this branch, render blocking of components have been dealt with by leveraging <span className="font-bold underline decoration-solid">Streaming</span> and <span className="font-bold underline decoration-solid">Suspense</span> in NextJS.</p>
+          <ul class="list-disc">
+            <li>By Streaming, you can prevent slow data requests from blocking your whole page.</li>
+            <li>This allows the user to see and interact with parts of the page without waiting for all the data to load before any UI can be shown to the user.</li>
+          </ul>
         </div>
       </div>
     </main>
